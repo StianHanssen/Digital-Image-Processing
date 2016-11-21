0dc8c7a11ebfbb1cbaa87d8fdac265cf8473d164
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-11-21 15:16:44 +0100

    Ready for test

--- project/area_segmentation.py
+++ project/area_segmentation.py
@@ -5,15 +5,24 @@ from wpa_testing import *
 from empty_check import *
 from collections import Counter
 from PIL import ImageEnhance, ImageFilter
+import sys
 
 color_to_shape = {(55, 45, 148): "star", (214, 71, 53): "pacman",
                   (217, 208, 199): "hexagon1", (59, 36, 36): "hexagon2",
                   (79, 122, 61): "sqare", (226, 201, 59): "v",
                   (176, 63, 119): "triangle"}
 
-def extract_piece(im, x, y):
-    x1, x2 = x * 100, (x + 1) * 100
-    y1, y2 = y * 100, (y + 1) * 100
+cmd_to_board = {"e1": "easy01.png", "e2": "easy01.png",
+                "d1": "difficult01.png", "d2": "difficult02.png"}
+
+color_to_name = {(55, 45, 148): "blue", (214, 71, 53): "red"}
+
+def extract_piece(im, pos, dimensions):
+    x, y = pos
+    width, height = im.size
+    p_width, p_height = width / dimensions[0], height / dimensions[1]
+    x1, x2 = x * p_width, (x + 1) * p_width
+    y1, y2 = y * p_height, (y + 1) * p_height
     im = im.crop((x1, y1, x2, y2)).crop((5, 5, 95, 95))
     return im
 
@@ -76,8 +85,8 @@ def get_center_pos(im, pos, size):
                 min_y = min(min_y, y)
                 max_x = max(max_x, x)
                 max_y = max(max_y, y)
-    r_x = int((max_x - min_x) / 2) + (b_x * width)
-    r_y = int((max_y - min_y) / 2) + (b_y * height)
+    r_x = int(((max_x - min_x) / 2) + (b_x * width))
+    r_y = int(((max_y - min_y) / 2) + (b_y * height))
     return r_x, r_y
 
 
@@ -102,19 +111,30 @@ def classify(im):
     return color_to_shape[shape_color]
 
 
+def get_background_color(im):
+    width, height = im.size
+    edge_colors = []
+    for i in range(width):
+        for j in range(height):
+            pixel = im.getpixel((i, j))
+            if i == 0 or j == 0 or i == width - 1 or j == height - 1:
+                edge_colors.append(pixel)
+    return Counter(edge_colors).most_common(1)[0][0]
+
 if __name__ == "__main__":
-    im1 = Image.open(getImagePath("difficult02.png"))
-    #im = map_color_wta(im1, False)
-    #im.show()
+    board = cmd_to_board[sys.argv[1]]
+    im1 = Image.open(getImagePath(board))
+    size = im1.size
     coords, cell_size = extract_coords(im1, (8, 5))
     pieces = extract_all(map_color_wta(im1).convert('L'), coords, (8, 5))
-    rgb_pieces = extract_all(map_color_wta(im1, False), coords, (8, 5))
+    color_board = map_color_wta(im1, False)
+    background = get_background_color(extract_piece(color_board, (0, 0), (8, 5)))
+    rgb_pieces = extract_all(color_board, coords, (8, 5))
     for i in range(len(pieces)):
         im, pos = pieces[i]
         rgb_im, _ = rgb_pieces[i]
-        #im.show()
+        if i == 0:
+            print(size, color_to_name[background])
         m = inverter(region_growing_method(im, tuple([(1, 1)]), 1))
         print(get_center_pos(m, pos, cell_size), classify(rgb_im))
         rgb_im.show()
-        m = from_bin_to_visual(m)
-        #m.show()
